008e1aa112f9fc684839f6d0314e0d3ba6c4c219
MukiriMwirigi <76058588+MukiriMwirigi@users.noreply.github.com>
2022-10-25 21:35:55 +0300

    structure

---
+++ blockchain/blockchain.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, requests 
+
+
+class Blockchain():
+
+    def __init__(self):
+        self.transactions = []
+        self.chain = []
+
+# instantiating the blockchain
+blockchain = Blockchain()
+
+# instantiating the node
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
